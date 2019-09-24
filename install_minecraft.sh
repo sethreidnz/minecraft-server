84bcc6d420ceaa46dd9ff58cfd683cfc1417f161
Seth <contact@sethreid.co.nz>
2019-09-24 18:13:03 +1200

    updated install script to work

--- install_minecraft.sh
+++ install_minecraft.sh
@@ -8,43 +8,24 @@ memoryAllocx=12g
 server_jar=Hexxit.jar
 mod_server_uri="http://servers.technicpack.net/Technic/servers/hexxit/Hexxit_Server_v1.0.10.zip"
 
-# add and update repos
-while ! echo y | apt-get install -y software-properties-common; do
-    sleep 10
-    apt-get install -y software-properties-common
-done
+DEBIAN_FRONTEND=noninteractive
+apt update
+apt install unzip
+apt install openjdk-8-jre-headless 
 
-while ! echo y | apt-add-repository -y ppa:linuxuprising/java; do
-    sleep 10
-    apt-add-repository -y ppa:linuxuprising/java
-done
-
-while ! echo y | apt-get update; do
-    sleep 10
-    apt-get update
-done
-
-# Install Java12
-echo oracle-java12-installer shared/accepted-oracle-license-v1-2 select true | /usr/bin/debconf-set-selections
-
-while ! echo y | apt-get install -y oracle-java12-installer; do
-    sleep 10
-    apt-get install -y oracle-java12-installer
-done
-
-# create user and install folder
+# # create user and install folder
+mkdir -p $minecraft_server_path;
+cd $minecraft_server_path;
 adduser --system --no-create-home --home $minecraft_server_path $minecraft_user
 addgroup --system $minecraft_group
-mkdir $minecraft_server_path
-cd $minecraft_server_path
 
 # download the server zip
 wget -qO- -O tmp.zip $mod_server_uri && unzip tmp.zip && rm tmp.zip
 
-# set permissions on install folder
+# # set permissions on install folder
 chown -R $minecraft_user $minecraft_server_path
 
-# create a service
+# # create a service
 touch /etc/systemd/system/minecraft-server.service
 printf '[Unit]\nDescription=Minecraft Service\nAfter=rc-local.service\n' >> /etc/systemd/system/minecraft-server.service
 printf '[Service]\nWorkingDirectory=%s\n' $minecraft_server_path >> /etc/systemd/system/minecraft-server.service
