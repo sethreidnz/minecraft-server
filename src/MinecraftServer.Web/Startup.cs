e670cdf46ba4310f02621f0d10ffddbbea01eaf6
Seth <contact@sethreid.co.nz>
2019-09-28 10:18:57 +1200

    added rules

--- src/MinecraftServer.Web/Startup.cs
+++ src/MinecraftServer.Web/Startup.cs
@@ -18,7 +18,7 @@ namespace MinecraftServer.Web
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public static void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
@@ -30,7 +30,7 @@ namespace MinecraftServer.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public static void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -39,6 +39,7 @@ namespace MinecraftServer.Web
             else
             {
                 app.UseExceptionHandler("/Error");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
