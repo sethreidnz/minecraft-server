e3604f6c0aad3d4dcb2c61868d957bef1d6a78ac
Seth <contact@sethreid.co.nz>
2019-12-20 18:58:05 +1300

    updated links ready for test

--- install_the-1122-pack.sh
+++ install_the-1122-pack.sh
@@ -5,8 +5,8 @@ minecraft_user=minecraft
 minecraft_group=minecraft
 memoryAllocs=6g
 memoryAllocx=8g
-server_jar=Hexxit.jar
-mod_server_uri="http://servers.technicpack.net/Technic/servers/hexxit/Hexxit_Server_v1.0.10.zip"
+server_jar=minecraft_server.1.12.2
+mod_server_uri="http://solder.endermedia.com/repository/downloads/the-1122-pack/the-1122-pack_1.0.1.zip"
 
 if [ ! -d "$minecraft_server_path" ]; then
 
@@ -23,21 +23,21 @@ if [ ! -d "$minecraft_server_path" ]; then
     addgroup --system $minecraft_group
 
     # download the server zip
-    wget -qO- -O tmp.zip "http://servers.technicpack.net/Technic/servers/hexxit/Hexxit_Server_v1.0.10.zip" && unzip tmp.zip && rm tmp.zip
+    wget -qO- -O tmp.zip "http://solder.endermedia.com/repository/downloads/the-1122-pack/the-1122-pack_1.0.1.zip" && unzip tmp.zip && rm tmp.zip
 
     # set permissions on server folder
-    chown -R minecraft /srv/minecraft_server
+    chown -R minecraft $minecraft_server_path
 
     # create a service to run minecraft
-    touch /etc/systemd/system/minecraft-server.service
-    printf '[Unit]\nDescription=Minecraft Service\nAfter=rc-local.service\n' >> /etc/systemd/system/minecraft-server.service
-    printf '[Service]\nWorkingDirectory=%s\n' $minecraft_server_path >> /etc/systemd/system/minecraft-server.service
-    printf 'ExecStart=/usr/bin/java -Xms%s -Xmx%s -jar %s/%s nogui\n' $memoryAllocs $memoryAllocx $minecraft_server_path $server_jar >> /etc/systemd/system/minecraft-server.service
-    printf 'ExecReload=/bin/kill -HUP $MAINPID\nKillMode=process\nRestart=on-failure\n' >> /etc/systemd/system/minecraft-server.service
-    printf '[Install]\nWantedBy=multi-user.target\nAlias=minecraft-server.service' >> /etc/systemd/system/minecraft-server.service
-    chmod +x /etc/systemd/system/minecraft-server.service
+    touch /etc/systemd/system/minecraft_the-1122-pack.service
+    printf '[Unit]\nDescription=Minecraft Service\nAfter=rc-local.service\n' >> /etc/systemd/system/minecraft_the-1122-pack.service
+    printf '[Service]\nWorkingDirectory=%s\n' $minecraft_server_path >> /etc/systemd/system/minecraft_the-1122-pack.service
+    printf 'ExecStart=/usr/bin/java -Xms%s -Xmx%s -jar %s/%s nogui\n' $memoryAllocs $memoryAllocx $minecraft_server_path $server_jar >> /etc/systemd/system/minecraft_the-1122-pack.service
+    printf 'ExecReload=/bin/kill -HUP $MAINPID\nKillMode=process\nRestart=on-failure\n' >> /etc/systemd/system/minecraft_the-1122-pack.service
+    printf '[Install]\nWantedBy=multi-user.target\nAlias=minecraft_the-1122-pack.service' >> /etc/systemd/system/minecraft_the-1122-pack.service
+    chmod +x /etc/systemd/system/minecraft_the-1122-pack.service
 
-    systemctl enable minecraft-server
-    systemctl start minecraft-server
+    systemctl enable minecraft_the-1122-pack
+    systemctl start minecraft_the-1122-pack
 
 fi
